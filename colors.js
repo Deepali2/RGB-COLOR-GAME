5ebc065bd29f1d997f91e42ab09ecbdc4ab79d3c
Deepali2 <hdgarg@gmail.com>
2018-10-28 19:23:19 -0700

    adds the reset button

--- colors.js
+++ colors.js
@@ -5,6 +5,27 @@ let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
+let resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function() {  
+  //generate all new colors
+  colors = generateRandomColors(6);
+  //pick a new random color from array
+  pickedColor = pickColor();
+  //change colorDisplay to match picked color
+  colorDisplay.textContent = pickedColor;
+  //change colors of squares
+  for (let i = 0; i < squares.length; i++) {  
+    //add initial colors to squares
+    squares[i].style.backgroundColor = colors[i];
+  }
+  //reset the text of the button to new colors
+  resetButton.textContent = "New Colors";
+  //reset the text of the message display to empty
+  messageDisplay.textContent = '';
+  //reset the background color of the h1 to the original color
+  h1.style.backgroundColor = "#232323";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -20,6 +41,7 @@ for (let i = 0; i < squares.length; i++) {
     //compare color to pickedColor
     if (clickedColor === pickedColor) {      
       messageDisplay.textContent = "Correct!";
+      resetButton.textContent = "PLAY AGAIN?";
       changeColors(pickedColor);
     } else {
       this.style.backgroundColor = "#232323";
